bda02cddb19363af17ee496111a2db224e774939
Kaisei Yoneyama <96284126+Kaisei-Yoneyama@users.noreply.github.com>
2025-03-26 23:17:03 +0900

    ESLint と Prettier で無視するファイルを指定

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
@@ -6,6 +6,7 @@ import pluginReact from "eslint-plugin-react";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
+  globalIgnores([".output/", ".wxt/"]),
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
