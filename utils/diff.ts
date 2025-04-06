16612ecd13661c91ad3f9e0ebe2ac504ad439448
Kaisei Yoneyama <97094702+mikrogeophagus@users.noreply.github.com>
2025-04-06 22:17:40 +0900

    /dev/null の判定を修正

--- utils/diff.ts
+++ utils/diff.ts
@@ -1,8 +1,8 @@
 import type { ParsedDiff } from "diff"
 
 export const getFilePath = ({ newFileName, oldFileName }: ParsedDiff) => {
-  if (newFileName === "dev/null") newFileName = undefined
-  if (oldFileName === "dev/null") oldFileName = undefined
+  if (newFileName === "/dev/null") newFileName = undefined
+  if (oldFileName === "/dev/null") oldFileName = undefined
 
   const filePathWithPrefix = newFileName ?? oldFileName ?? ""
   const filePath = filePathWithPrefix.replace(/^[ab]\//, "")
