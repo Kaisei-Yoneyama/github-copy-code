bdb72fbfc237305645fccbca7c819243bae344c4
Kaisei Yoneyama <96284126+Kaisei-Yoneyama@users.noreply.github.com>
2025-08-06 01:44:27 +0900

    テストワークフローを実装

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: "html",
+  reporter: process.env.CI ? [["dot"], ["github"]] : "html",
   use: {
     trace: "on-first-retry",
   },
