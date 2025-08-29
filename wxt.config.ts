c84e30009e847eace564324ccaaeb6e3704bc140
Kaisei Yoneyama <96284126+Kaisei-Yoneyama@users.noreply.github.com>
2025-08-30 03:24:33 +0900

    ブラウザー起動時の開始ページを指定

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from "wxt"
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  webExt: {
+    startUrls: [
+      "https://github.com/Kaisei-Yoneyama/github-copy-code/pull/4/files",
+      "https://github.com/Kaisei-Yoneyama/github-copy-code/pull/4/commits/60697586657907de592a34eee735f7b56079fd55",
+      "https://github.com/Kaisei-Yoneyama/github-copy-code/commit/60697586657907de592a34eee735f7b56079fd55",
+    ],
+  },
   modules: ["@wxt-dev/module-react"],
   manifest: {
     host_permissions: ["*://*.github.com/*", "*://*.githubusercontent.com/*"],
